90be06f174d5cf6fd610c5793dc87cbd8a26793b
Houssam almomani <92177957+houssamalmomani@users.noreply.github.com>
2023-02-02 14:22:33 +0300

    Update CategoriesData.tsx

--- components/categories/CategoriesData.tsx
+++ components/categories/CategoriesData.tsx
@@ -8,7 +8,7 @@ const categoriesData: (string | StaticImageData)[][] = [
 		'/fliped5.jpg',
 	],
 	['Necklaces', '/categories/necklaces', '/necklaces.jpg', '/fliped2.jpg'],
-	['Key Chains', '/categories/keychains', '/keyD1.jpg', '/keym.jpg'],
+	['Key Chains', '/categories/keychains', '/keyD1.jpg', '/keyM.jpg'],
 ];
 
 export default categoriesData;
